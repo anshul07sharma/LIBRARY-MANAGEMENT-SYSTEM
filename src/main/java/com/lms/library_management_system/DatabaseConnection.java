c5c6d35e77332758a6c811b3ab2a7fb0ea5dd612
anshul07sharma <165197445+anshul07sharma@users.noreply.github.com>
2024-09-15 03:56:41 +0530

    Update DatabaseConnection.java

--- src/main/java/com/lms/library_management_system/DatabaseConnection.java
+++ src/main/java/com/lms/library_management_system/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection
 {
     private static final String URL = "jdbc:mysql://localhost:3306/library_management_system";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Ankit@123";
+    private static final String PASSWORD = "Your Password";
 
     public static Connection getConnection() 
     {
